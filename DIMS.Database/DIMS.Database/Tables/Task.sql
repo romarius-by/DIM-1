900579caff2602c9be18c6cc59f626f9402f150b
VladRossohin <rossohin647@mail.ru>
2020-02-02 17:12:05 +0300

    add UserProgress view, Task & TaskTrack tables

---
+++ DIMS.Database/DIMS.Database/Tables/Task.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[TaskId] INT IDENTITY NOT NULL , 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(256) NOT NULL, 
+    [StartDate] DATE NOT NULL, 
+    [DeadlineDate] DATE NOT NULL
+
+	CONSTRAINT [PK_Task] PRIMARY KEY ([TaskId])
+)
--- DIMS.Database/DIMS.Database/Views/vUserProgress.sql
+++ DIMS.Database/DIMS.Database/Views/vUserProgress.sql
@@ -1,9 +1,9 @@
-CREATE VIEW [dbo].[vUserProgress]
+﻿CREATE VIEW [dbo].[vUserProgress]
 	AS SELECT ISNULL(up.UserId, -999) AS UserId,
-	ISNULL(t.TaskId, -999) AS TaskId,
+	ISNULL(ut.TaskId, -999) AS TaskId,
 	ISNULL(tr.TaskTrackId, -999) AS TaskTrackId,
 	CONCAT(up.Name, ' ', up.LastName) AS UserName,
 	t.Name AS TaskName,
 	tr.TrackNote AS TrackNote,
 	tr.TrackDate AS TrackDate
-	FROM [UserProfile] up, [Task] t, [TaskTrack] tr
+	FROM [UserProfile] up JOIN [UserTask] ut ON up.UserId=ut.UserId JOIN [TaskTrack] tr ON ut.TaskId=tr.UserTaskId JOIN [Task] t ON ut.TaskId=t.TaskId

11516e60d0389253d378bc225da6b3bc9830fac5
VladRossohin <rossohin647@mail.ru>
2020-02-03 21:08:58 +0300

    Add TaskRepository

---
+++ DIMS/DIMS.DAL.Data/Repositories/TaskRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.EF.DAL.Data.Repositories
+{
+    class TaskRepository : IRepository<Task>
+    {
+        private readonly DIMSDBContext _dIMSDBContext;
+
+        public TaskRepository(DIMSDBContext dIMSDBContext)
+        {
+            _dIMSDBContext = dIMSDBContext;
+        }
+
+        public void Create(Task item)
+        {
+            _dIMSDBContext.Tasks.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            Task task = _dIMSDBContext.Tasks.Find(id);
+
+            if (task != null)
+            {
+                _dIMSDBContext.Tasks.Remove(task);
+            }
+        }
+
+        public IEnumerable<Task> Find(Func<Task, bool> predicate)
+        {
+            return _dIMSDBContext.Tasks.Where(predicate).ToList();
+        }
+
+        public Task Get(int id)
+        {
+            return _dIMSDBContext.Tasks.Find(id);
+        }
+
+        public IEnumerable<Task> GetAll()
+        {
+            return _dIMSDBContext.Tasks;
+        }
+
+        public void Update(Task item)
+        {
+            _dIMSDBContext.Entry(item).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+}

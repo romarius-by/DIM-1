428f26c2567118c78d2faf68f53c2fe40d0b76ea
VladRossohin <rossohin647@mail.ru>
2020-02-14 21:27:57 +0300

    add direction service

---
+++ DIMS/DIMS.DAL.Data/Repositories/DirectionRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.EF.DAL.Data.Repositories
+{
+    public class DirectionRepository : IRepository<Direction>
+    {
+        private readonly DIMSDBContext _dIMSDBContext;
+
+        public DirectionRepository(DIMSDBContext dIMSDBContext)
+        {
+            _dIMSDBContext = dIMSDBContext;
+        }
+
+        public void Create(Direction item)
+        {
+            _dIMSDBContext.Directions.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            var direction = _dIMSDBContext.Directions.Find(id);
+
+            if (direction != null)
+            {
+                _dIMSDBContext.Directions.Remove(direction);
+            }
+        }
+
+        public IEnumerable<Direction> Find(Func<Direction, bool> predicate)
+        {
+            return _dIMSDBContext.Directions.Where(predicate).ToList();
+        }
+
+        public Direction Get(int id)
+        {
+            return _dIMSDBContext.Directions.Find(id);
+        }
+
+        public IEnumerable<Direction> GetAll()
+        {
+            return _dIMSDBContext.Directions;
+        }
+
+        public void Update(Direction item)
+        {
+            _dIMSDBContext.Entry(item).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+}

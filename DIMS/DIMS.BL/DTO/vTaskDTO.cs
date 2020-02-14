568d71dd5f18664f8f3c8241cd3fa31d49eb53f5
VladRossohin <rossohin647@mail.ru>
2020-02-14 20:29:44 +0300

    add view DTOs

---
+++ DIMS/DIMS.BL/DTO/vTaskDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.DTO
+{
+    public class vTaskDTO
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime DeadlineDate { get; set; }
+
+
+    }
+}

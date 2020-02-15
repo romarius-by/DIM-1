7394e24ecda4f132accf032adc9470d908345987
VladRossohin <rossohin647@mail.ru>
2020-02-15 20:33:35 +0300

    add view user progress service

---
+++ DIMS/DIMS.BL/Interfaces/IvUserProgressService.cs
@@ -0,0 +1,17 @@
+﻿using HIMS.BL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Interfaces
+{
+    public interface IvUserProgressService
+    {
+        vUserProgressDTO GetVUserProgress(int? id);
+        ICollection<vUserProgressDTO> GetVUserProfiles();
+
+        void Dispose();
+    }
+}

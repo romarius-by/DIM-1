a574cfaad26bb88c95610de59d7d8a37c75cf170
VladRossohin <rossohin647@mail.ru>
2020-02-15 20:33:35 +0300

    add view user progress service

---
+++ DIMS/DIMS.BL/Services/vUserProgressService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using HIMS.BL.DTO;
+using HIMS.BL.Infrastructure;
+using HIMS.BL.Interfaces;
+using HIMS.EF.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Services
+{
+    public class vUserProgressService : IvUserProgressService
+    {
+
+        private IUnitOfWork Database;
+
+        public vUserProgressService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+
+        public ICollection<vUserProgressDTO> GetVUserProfiles()
+        {
+            return Mapper.Map<List<vUserProgress>, ICollection<vUserProgressDTO>>(
+                Database.vUserProgresses.GetAll().ToList());
+        }
+
+        public vUserProgressDTO GetVUserProgress(int? id)
+        {
+            if (!id.HasValue)
+                throw new ValidationException("The view user progress id value is not set", String.Empty);
+
+            var _vUserProgress = Database.vUserProgresses.Get(id.Value);
+
+            if (_vUserProgress == null)
+                throw new ValidationException($"The view user progress with id = {id.Value} was not found", String.Empty);
+
+            return Mapper.Map<vUserProgress, vUserProgressDTO>(_vUserProgress);
+
+        }
+    }
+}

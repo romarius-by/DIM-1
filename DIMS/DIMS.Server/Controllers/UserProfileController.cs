9581b7ef2996083b2c4bf4d57346520f693b85a9
VladRossohin <rossohin647@mail.ru>
2020-02-15 22:25:47 +0300

    init user profile controller

---
+++ DIMS/DIMS.Server/Controllers/UserProfileController.cs
@@ -0,0 +1,23 @@
+﻿using HIMS.BL.DTO;
+using HIMS.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HIMS.Server.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private readonly IUserProfileService _userProfileService;
+
+        public UserProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+
+
+    }
+}

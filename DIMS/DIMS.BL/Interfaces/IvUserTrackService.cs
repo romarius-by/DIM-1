3dd265e30cd19624e77be7259a51d04e4ec16695
VladRossohin <rossohin647@mail.ru>
2020-02-15 20:43:42 +0300

    add view user track service

---
+++ DIMS/DIMS.BL/Interfaces/IvUserTrackService.cs
@@ -0,0 +1,17 @@
+﻿using HIMS.BL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Interfaces
+{
+    public interface IvUserTrackService
+    {
+        vUserTrackDTO GetVUserTrack(int? id);
+        ICollection<vUserTrackDTO> GetVUserTracks();
+
+        void Dispose();
+    }
+}

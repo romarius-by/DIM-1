30ce3c91699437ce287d1ee9bd948ceaf725601f
VladRossohin <rossohin647@mail.ru>
2020-02-15 19:22:18 +0300

    add view task interface

---
+++ DIMS/DIMS.BL/Interfaces/IvTaskService.cs
@@ -0,0 +1,16 @@
+﻿using HIMS.BL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Interfaces
+{
+    public interface IvTaskService
+    {
+        vTaskDTO GetVTask(int? id);
+        List<vTaskDTO> GetVTasks();
+        void Dispose();
+    }
+}

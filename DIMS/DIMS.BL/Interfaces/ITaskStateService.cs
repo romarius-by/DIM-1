024de515031edbdbdb83f9d3028ea369e2ab346d
VladRossohin <rossohin647@mail.ru>
2020-02-15 15:43:24 +0300

    add task state service

---
+++ DIMS/DIMS.BL/Interfaces/ITaskStateService.cs
@@ -0,0 +1,19 @@
+﻿using HIMS.BL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Interfaces
+{
+    public interface ITaskStateService
+    {
+        void SaveTaskState(TaskStateDTO taskStateDTO);
+        TaskStateDTO GetTaskState(int? id);
+        void UpdateTaskState(TaskStateDTO taskStateDTO);
+        void DeleteTaskState(int? id);
+
+        ICollection<UserTaskDTO> GetUserTasks(int? id); 
+    }
+}

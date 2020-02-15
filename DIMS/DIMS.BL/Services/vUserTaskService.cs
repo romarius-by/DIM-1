12f2ff83b747b24f694d5377206de1b2e94f655c
VladRossohin <rossohin647@mail.ru>
2020-02-15 20:38:43 +0300

    add view user task service

---
+++ DIMS/DIMS.BL/Services/vUserTaskService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using HIMS.BL.DTO;
+using HIMS.BL.Infrastructure;
+using HIMS.BL.Interfaces;
+using HIMS.EF.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Services
+{
+    public class vUserTaskService : IvUserTaskService
+    {
+
+        private IUnitOfWork Database;
+
+        public vUserTaskService(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+
+        public vUserTaskDTO GetUserTask(int? id)
+        {
+            if (!id.HasValue)
+                throw new ValidationException("The view user task id value is not set", String.Empty);
+
+            var _vUserTask = Database.vUserTasks.Get(id.Value);
+
+            if (_vUserTask == null)
+                throw new ValidationException($"The view user task with id = {id.Value} was not found", String.Empty);
+
+            return Mapper.Map<vUserTask, vUserTaskDTO>(_vUserTask);
+        }
+
+        public ICollection<vUserTaskDTO> GetVUserTasks()
+        {
+            return Mapper.Map<List<vUserTask>, ICollection<vUserTaskDTO>>(
+                Database.vUserTasks.GetAll().ToList());
+        }
+    }
+}

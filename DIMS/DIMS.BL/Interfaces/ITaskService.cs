e3fe602ac22cea9802ce55aacf98fe8d80bd926d
VladRossohin <rossohin647@mail.ru>
2020-02-15 15:17:59 +0300

    add task interface

---
+++ DIMS/DIMS.BL/Interfaces/ITaskService.cs
@@ -0,0 +1,21 @@
+﻿using HIMS.BL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.BL.Interfaces
+{
+    public interface ITaskService
+    {
+        void SaveTask(TaskDTO task);
+        TaskDTO GetTask(int? id);
+        void UpdateTask(TaskDTO taskDTO);
+        void DeleteTask(int? id);
+
+        IEnumerable<TaskDTO> GetTasks();
+        IEnumerable<UserTaskDTO> GetUserTasks(int? id);
+        void Dispose();
+    }
+}

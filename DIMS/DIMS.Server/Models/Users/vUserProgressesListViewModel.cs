7420e850e29f81eb754ef22c418a1ea851a96b8e
VladRossohin <rossohin647@mail.ru>
2020-03-03 16:05:39 +0300

    add userProgress view grid

---
+++ DIMS/DIMS.Server/Models/Users/vUserProgressesListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HIMS.Server.Models.Users
+{
+    public class vUserProgressesListViewModel
+    {
+        public vUserProfileViewModel vUserProfile { get; set; }
+        public IEnumerable<vUserProgressViewModel> vUserProgresses { get; set; }
+    }
+}

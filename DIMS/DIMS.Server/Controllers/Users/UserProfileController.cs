8f518075b44560f7e8e265fe5733e3fde4571ee2
VladRossohin <rossohin647@mail.ru>
2020-03-08 19:15:33 +0300

    add progress view for users

--- DIMS/DIMS.Server/Controllers/Users/UserProfileController.cs
+++ DIMS/DIMS.Server/Controllers/Users/UserProfileController.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -19,14 +20,25 @@ namespace HIMS.Server.Controllers
         private readonly IUserProfileService _userProfileService;
         private readonly IvUserProfileService _vUserProfileService;
         private readonly IDirectionService _directionService;
+        private readonly IvUserProgressService _vUserProgressService;
 
         private UserProfilePageViewModel UserProfilesPageViewModel;
-        public UserProfileController(IUserProfileService userProfileService, IvUserProfileService vuserProfileService, IDirectionService directionService)
+        public UserProfileController(IUserProfileService userProfileService, IvUserProfileService vuserProfileService, IDirectionService directionService, IvUserProgressService vUserProgressService)
         {
             _userProfileService = userProfileService;
             _vUserProfileService = vuserProfileService;
             _directionService = directionService;
+            _vUserProgressService = vUserProgressService;
+
             UserProfilesPageViewModel = new UserProfilePageViewModel();
+           /* UserProfilesPageViewModel = new UserProfilePageViewModel
+            {
+                UserProfileViewModel = new UserProfileViewModel(),
+                UserProfilesListViewModel = new UserProfilesListViewModel(),
+                vUserProfilesListViewModel = new vUserProfilesListViewModel(),
+                vUserProfileViewModel = new vUserProfileViewModel(),
+                vUserProgressesListViewModel = new vUserProgressesListViewModel()
+            };*/
         }
 
 
@@ -59,10 +71,9 @@ namespace HIMS.Server.Controllers
 
         public ActionResult Create(UserProfileViewModel userProfileViewModel)
         {
-            UserProfilesPageViewModel.UserProfileViewModel = userProfileViewModel;
             UserProfilesPageViewModel.DirectionViewModels = Mapper.Map<IEnumerable<DirectionDTO>, List<DirectionViewModel>>
                 (_directionService.GetDirections());
-            return View(UserProfilesPageViewModel);
+            return PartialView(UserProfilesPageViewModel);
         }
 
         [HttpPost]
@@ -71,6 +82,7 @@ namespace HIMS.Server.Controllers
         {
             try
             {
+                var UserProfileDTO = Mapper.Map<UserProfileViewModel, UserProfileDTO>(UserProfilesPageViewModel.UserProfileViewModel);
                 if (ModelState.IsValid)
                 {
                     var userProfileDTO = Mapper.Map<UserProfileViewModel, UserProfileDTO>(userProfileViewModel);
@@ -88,13 +100,76 @@ namespace HIMS.Server.Controllers
             }
 
 
+            return View(UserProfilesPageViewModel);
+        }
+
+        public ActionResult Edit(UserProfileViewModel userProfileViewModel, int? id)
+        {
+            if (!id.HasValue)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var userProfileDto = _userProfileService.GetUserProfileById(id.Value);
+
+            if (userProfileDto == null)
+                return HttpNotFound();
+
+            ViewBag.User = (UserProfileViewModel)Mapper.Map<UserProfileDTO, UserProfileViewModel>(userProfileDto);
+           // UserProfilesPageViewModel.UserProfileViewModel =
+             //   Mapper.Map<UserProfileDTO, UserProfileViewModel>(userProfileDto);
+            UserProfilesPageViewModel.DirectionViewModels =
+                Mapper.Map<IEnumerable<DirectionDTO>, List<DirectionViewModel>>
+                (_directionService.GetDirections());
+            
+            return View(UserProfilesPageViewModel);
+        }
+
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProfile(int? id)
+        {
+            if (!id.HasValue)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var userProfileDto = _userProfileService.GetUserProfileById(id.Value);
+
+            if (TryUpdateModel(userProfileDto, "",
+                new string[] {
+                      nameof(userProfileDto.Name),
+                      nameof(userProfileDto.LastName),
+                      nameof(userProfileDto.Email),
+                      nameof(userProfileDto.Address),
+                      nameof(userProfileDto.DirectionId),
+                      nameof(userProfileDto.BirthDate),
+                      nameof(userProfileDto.Education),
+                      nameof(userProfileDto.MathScore),
+                      nameof(userProfileDto.UniversityAverageScore),
+                      nameof(userProfileDto.MobilePhone),
+                      nameof(userProfileDto.Sex),
+                      nameof(userProfileDto.Skype),
+                      nameof(userProfileDto.StartDate)
+                }))
+            {
+                try
+                {
+                    _userProfileService.UpdateUserProfile(userProfileDto);
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+
+            UserProfilesPageViewModel.UserProfileViewModel = Mapper.Map<UserProfileDTO, UserProfileViewModel>(userProfileDto);
+
             return PartialView(UserProfilesPageViewModel);
         }
 
         public ActionResult Details(int? id)
         {
             if (!id.HasValue)
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
             var vuserProfileDto = _vUserProfileService.GetVUserProfile(id.Value);
 
@@ -104,6 +179,21 @@ namespace HIMS.Server.Controllers
             var vuserProfile = Mapper.Map<vUserProfileDTO, vUserProfileViewModel>(vuserProfileDto);
 
             return PartialView(vuserProfile);
+        }
+
+        public ActionResult Progress(int? id)
+        {
+            if (!id.HasValue)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            UserProfilesPageViewModel.vUserProgressesListViewModel = new vUserProgressesListViewModel(
+                Mapper.Map<vUserProfileDTO, vUserProfileViewModel>(
+                    _vUserProfileService.GetVUserProfile(id.Value)),
+                Mapper.Map<IEnumerable<vUserProgressDTO>, IEnumerable<vUserProgressViewModel>>(
+                _vUserProgressService.GetVUserProgressesByUserId(id.Value))
+                );
+
+            return PartialView(UserProfilesPageViewModel);
         }
 
 

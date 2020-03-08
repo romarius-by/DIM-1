5c0cb21d4a607d6279251bf36e4414ee342d5036
VladRossohin <rossohin647@mail.ru>
2020-03-08 19:15:33 +0300

    add progress view for users

--- DIMS/DIMS.Server/Models/Users/UserProfilePageViewModel.cs
+++ DIMS/DIMS.Server/Models/Users/UserProfilePageViewModel.cs
@@ -8,10 +8,13 @@ namespace HIMS.Server.Models.Users
 {
     public class UserProfilePageViewModel
     {
+        
+
         public UserProfileViewModel UserProfileViewModel { get; set; }
         public UserProfilesListViewModel UserProfilesListViewModel { get; set; }
         public vUserProfileViewModel vUserProfileViewModel { get; set; }
         public vUserProfilesListViewModel vUserProfilesListViewModel { get; set; }
+        public vUserProgressesListViewModel vUserProgressesListViewModel { get; set; }
 
         public IEnumerable<DirectionViewModel> DirectionViewModels { get; set; }
     }
--- DIMS/DIMS.Server/Models/Users/vUserProgressesListViewModel.cs
+++ DIMS/DIMS.Server/Models/Users/vUserProgressesListViewModel.cs
@@ -7,6 +7,12 @@ namespace HIMS.Server.Models.Users
 {
     public class vUserProgressesListViewModel
     {
+        public vUserProgressesListViewModel(vUserProfileViewModel vUserProfile, IEnumerable<vUserProgressViewModel> vUserProgresses)
+        {
+            this.vUserProfile = vUserProfile;
+            this.vUserProgresses = vUserProgresses;
+        }
+
         public vUserProfileViewModel vUserProfile { get; set; }
         public IEnumerable<vUserProgressViewModel> vUserProgresses { get; set; }
     }

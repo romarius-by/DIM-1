a8d01ce0e73937cbe00576f83eede7a80c844371
maximsan <maximuzz@yandex.ru>
2020-05-04 02:30:51 +0300

    add common dll and LinqExtensions class

---
+++ DIMS/DIMS.Common/LinqExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DIMS.Common
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> ToListBy<T>(this IEnumerable<T> items, Predicate<T> whereProvider)
+        {
+            return items.Where(item => whereProvider(item)).ToList();
+        } 
+    }
+}

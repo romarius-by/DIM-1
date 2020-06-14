fce91e0bcabd7837d5f753d5392ca4e74c0bb4c5
romarius-by <romarius-by@users.noreply.github.com>
2020-06-14 22:31:49 +0300

    added partial view for modal buttons

---
+++ DIMS/DIMS.Server/Models/Partials/ModalButtonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DIMS.Server.Models.Partials
+{
+    public class ModalButtonViewModel
+    {
+        public string Value { get; set; }
+        public string ButtonClass { get; set; }
+        public bool IsItDetails { get; set; }
+        public int? Id { get; set; }
+    }
+}
